c673e23ca92f2c91b1512474d387cc156d3e95c8
BENJAMIN DOKU <benjibd@gmail.com>
2021-05-01 19:04:57 +0000

    git commit "updated html locator"
    updated html locator
    commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { Builder, By } = require("selenium-webdriver");
+const { Builder, By ,until} = require("selenium-webdriver");
 const faker = require('faker');
 const moment = require('moment');
 const driver = new Builder()
   .forBrowser("chrome")
   .build();
 
-const url = "http://192.168.4.124:3000"
+const url = "http://192.168.8.117:3000/"
 
 async function createPatientsAllFields() {
 
@@ -21,20 +21,24 @@ async function createPatientsAllFields() {
       let dob = faker.date.between('1980-01-01', '2021-03-30')
       dob = moment(dob).format('YYYY-MM-DD')
       let phoneNum =faker.phone.phoneNumber('0244######')
+      let fullName = fName + " " + mName + " " + lName
       await driver.findElement(By.css("input[data-test-id = 'first-name']")).sendKeys(fName)
       await driver.findElement(By.css("input[data-test-id = 'last-name']")).sendKeys(lName)
       await driver.findElement(By.css("input[data-test-id = 'middle-name']")).sendKeys(mName)
       await driver.findElement(By.css("input[data-test-id = 'phone-number']")).sendKeys(phoneNum)
       await driver.findElement(By.css("textarea[data-test-id = 'address']")).sendKeys(address)
       await driver.findElement(By.css("input[data-test-id = 'dob']")).sendKeys(dob)
-     
       await driver.findElement(By.css("a[data-test-id = 'submit-btn']")).click()
-      let element = await (await driver.findElement(By.xpath("/html/body/div/div/div[2]/main/div/div[2]/h4"))).getText()
-      if (element == (fName + " " + mName + " " + lName)) {
+     
+      let name = await (await driver.findElement(By.xpath("/html/body/div/div/div[2]/main/div/div[2]/h4"))).getText();
+
+      if (fullName == name) {
         console.log("createPatientsAllFields: Pass")
       } else {
         console.log("createPatientsAllFields: Fail")
       }
+
+    
 
   
 
@@ -42,7 +46,7 @@ async function createPatientsAllFields() {
     console.log(console.log("createPatientsAllFields: Fail"))
   }
 
-  await driver.quit()
+  await driver.close()
 
 }
 
@@ -53,7 +57,7 @@ async function createPatientRequiredFieldsOnly() {
 
     await driver.get(url)
 
-    let fName = faker.name.firstName()
+    let fName = faker.name.firstName() 
     let lName = faker.name.lastName()  
     let dob = faker.date.between('1980-01-01', '2021-03-30')
     dob = moment(dob).format('YYYY-MM-DD')
@@ -63,7 +67,7 @@ async function createPatientRequiredFieldsOnly() {
     await driver.findElement(By.css("input[data-test-id = 'phone-number']")).sendKeys(phoneNum)
     await driver.findElement(By.css("input[data-test-id = 'dob']")).sendKeys(dob)
     await driver.findElement(By.css("a[data-test-id = 'submit-btn']")).click()
-    let name = await driver.findElement(By.xpath("/html/body/div/div/div[2]/main/div/div[2]/h4")).getText()
+    let name = await (await driver.findElement(By.xpath("/html/body/div/div/div[2]/main/div/div[2]/h4"))).getText();
    
 
    
@@ -77,10 +81,10 @@ async function createPatientRequiredFieldsOnly() {
 
 
   } catch (error) {
-    console.log("+++++++++createSinglePatientRequiredFieldsOnly: Fail",error)
+    console.log("createSinglePatientRequiredFieldsOnly: Fail",error)
   }
 
-  
+ //await driver.close()
 
 }
 
@@ -92,9 +96,3 @@ createPatientRequiredFieldsOnly().then(() => {
 
 
 }).catch((err)=> console.log(err))
-
-
-
-
-
-  //runTestcases()
